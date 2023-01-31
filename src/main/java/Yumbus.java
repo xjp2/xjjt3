37a045c0f6bdbd74a588712f3a9c987550fadef9
xjp2 <xiujee02@gmail.com>
2023-01-31 23:45:15 +0800

    test

--- src/main/java/Yumbus.java
+++ src/main/java/Yumbus.java
@@ -23,7 +23,7 @@ public class Yumbus extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	//Step 1: Prepare list of variables used for database connections
-		private String jdbcURL = "jdbc:mysql://localhost:3306/yumbus";
+		private String jdbcURL = "jdbc:mysql://localhost:3306/test";
 		private String jdbcUsername = "root";
 		private String jdbcPassword = "password";
 		
